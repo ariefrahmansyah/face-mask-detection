2e693b683d3841684a9168ffe32d2f1a4f0a9861
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2020-06-27 11:04:16 +0700

    Update UI: add with or withous mask label on live webcam

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ import * as blazeface from '@tensorflow-models/blazeface';
 let video, videoWidth, videoHeight, webcam;
 async function setupCamera() {
   video = document.getElementById('video');
-  // try {
-  //   webcam = await tf.data.webcam(video);
-  // } catch (e) {
-  //   console.error("init webcam:", e);
-  // }
 
   const stream = await navigator.mediaDevices.getUserMedia({
     'audio': false,
@@ -51,21 +46,18 @@ async function loadMaskDetectionModel() {
   });
 }
 
-async function getImage() {
-  const img = await webcam.capture();
-  // const processedImg = tf.tidy(() => img.expandDims(0).toFloat().div(127).sub(1));
-  // img.dispose();
-  return img;
-}
-
 const returnTensors = false;
 const flipHorizontal = true;
 const annotateBoxes = false;
 const offset = tf.scalar(127.5);
 
+const decisionThreshold = 0.95;
+
 async function renderPrediction() {
+  // Get image from webcame
   let img = tf.tidy(() => tf.browser.fromPixels(video));
 
+  // Detect faces
   let faces = [];
   try {
     faces = await faceDetectionModel.estimateFaces(img, returnTensors, flipHorizontal, annotateBoxes);
@@ -76,6 +68,8 @@ async function renderPrediction() {
 
   // console.log("faces", faces);
 
+  // Loop through all predicted faces and detect if mask used or not.
+  // Then highlight the faces into the live view.
   if (faces.length > 0) {
     for (let i = 0; i < faces.length; i++) {
       let predictions = [];
@@ -96,12 +90,23 @@ async function renderPrediction() {
       const end = faces[i].bottomRight;
       const size = [end[0] - start[0], end[1] - start[1]];
 
-      canvasCtx.fillStyle = "rgba(255, 0, 0, 0.5)";
-      if (predictions.length > 0 && predictions[0] > predictions[1]) {
-        canvasCtx.fillStyle = "rgba(0, 255, 0, 0.5)";
+      canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+
+      canvasCtx.fillStyle = "rgba(255, 111, 0, 0.85)";
+      canvasCtx.fillRect(start[0], start[1] - 23, size[0], 23);
+
+      let label = "without mask";
+      let boxStyle = "rgba(255, 0, 0, 0.25)";
+      if (predictions.length > 0 && predictions[0] > decisionThreshold) {
+        label = `with mask: ${Math.round(predictions[0] * 100)}%`;
+        boxStyle = "rgba(0, 255, 0, 0.25)";
       }
 
-      canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+      canvasCtx.font = "18px Raleway";
+      canvasCtx.fillStyle = "rgba(255, 255, 255, 1)";
+      canvasCtx.fillText(label, end[0] + 5, start[1] - 5);
+
+      canvasCtx.fillStyle = boxStyle;
       canvasCtx.fillRect(start[0], start[1], size[0], size[1]);
     }
   }
